ca38569e865a4c36c7f47599f7a2aee55f712f8d
leeyegy <leeyegy@gmail.com>
2020-10-07 21:49:11 +0800

    add camera

--- deploy/take_pic.py
+++ deploy/take_pic.py
@@ -7,51 +7,35 @@ from datetime import datetime
 
 from PIL import Image
 import numpy as np
+import os
 
 parser = argparse.ArgumentParser(description='take a picture')
 parser.add_argument('--name', '-n', default='unknown', type=str, help='input the name of the recording person')
 args = parser.parse_args()
 
 data_path = Path('data')
-save_path = data_path / 'facebank' / args.name
-if not save_path.exists():
-    save_path.mkdir()
+save_path = os.path.join(data_path, 'facebank' , args.name)
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
 
-# 初始化摄像头
-cap = cv2.VideoCapture(0)
-# 我的摄像头默认像素640*480，可以根据摄像头素质调整分辨率
-cap.set(3, 1280)
-cap.set(4, 720)
-# mtcnn = MTCNN()
+import cv2
+import time
 
-while cap.isOpened():
-    # 采集一帧一帧的图像数据
-    isSuccess, frame = cap.read()
-    # 实时的将采集到的数据显示到界面上
-    if isSuccess:
-        frame_text = cv2.putText(frame,
-                                 'Press t to take a picture,q to quit.....',
-                                 (10, 100),
-                                 cv2.FONT_HERSHEY_SIMPLEX,
-                                 2,
-                                 (0, 255, 0),
-                                 3,
-                                 cv2.LINE_AA)
-        cv2.imshow("My Capture", frame_text)
-    # 实现按下“t”键拍照
-    if cv2.waitKey(1) & 0xFF == ord('t'):
-        p = Image.fromarray(frame[..., ::-1])
-        try:
-            # warped_face = np.array(mtcnn.align(p))[..., ::-1]
-            warped_face = np.array(p)
-            cv2.imwrite(str(save_path / '{}.jpg'.format(str(datetime.now())[:-7].replace(":", "-").replace(" ", "-"))),
-                        warped_face)
-        except:
-            print('no face captured')
+cap=cv2.VideoCapture(0)
+cap.set(3,1280)
+cap.set(4,720)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+while(cap.isOpened()):
+    ret_flag, Vshow = cap.read()
+    cv2.imshow('Capture', Vshow)
+    k=cv2.waitKey(1)
+    if k==ord('s'):
+        save_file_name = os.path.join(save_path,'{}.jpg'.format(str(datetime.now())[:-7].replace(":", "-").replace(" ", "-")))
+        cv2.imwrite(save_file_name,np.asarray(Vshow))
+        print(cap.get(3))
+        print(cap.get(4))
+    elif k==ord('q'):
+        print('完成')
         break
-
-# release
 cap.release()
 cv2.destoryAllWindows()
